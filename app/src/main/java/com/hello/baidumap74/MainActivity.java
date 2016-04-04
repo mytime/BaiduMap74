45db115e9c800fa4907acd00010dbfb8c745a0b1
mytime <cmba@qq.com>
2016-04-04 14:27:21 +0800

    更新地图状态
    设置放大，缩小，旋转，俯仰，移动

--- app/src/main/java/com/hello/baidumap74/MainActivity.java
+++ app/src/main/java/com/hello/baidumap74/MainActivity.java
@@ -72,7 +72,41 @@ public class MainActivity extends BaseActivity {
 
 
     }
-    
+    //5 更新地图状态
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        MapStatusUpdate update = null;
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_1://缩小
+                update = MapStatusUpdateFactory.zoomOut();
+                break;
+            case KeyEvent.KEYCODE_2://放大
+                update = MapStatusUpdateFactory.zoomIn();
+                break;
+            case KeyEvent.KEYCODE_3://每次在原来的基础上再旋转30度(0~360)
+                MapStatus currentMapStatus = map.getMapStatus(); //地图当前状态
+                float rotate = currentMapStatus.rotate + 30; //当前状态再加30度
+                Log.i(TAG, "角度：" + rotate);
+                MapStatus mapStatus = new MapStatus.Builder().rotate(rotate).build();
+                update = MapStatusUpdateFactory.newMapStatus(mapStatus);
+                break;
+            case KeyEvent.KEYCODE_4://每次在原来的基础上再俯仰-5度(0~45);
+                float overlook = map.getMapStatus().overlook;
+                float v = overlook-5;
+                mapStatus = new MapStatus.Builder().overlook(v).build();
+                update = MapStatusUpdateFactory.newMapStatus(mapStatus);
+                break;
+            case KeyEvent.KEYCODE_5://移动 到 中信广场 坐标 113.330352,23.148398
+                LatLng zxFoc = new LatLng(23.148398,113.330352);
+                update = MapStatusUpdateFactory.newLatLng(zxFoc);
+                map.animateMapStatus(update,2000); //用动画形式移动坐标
+                return super.onKeyDown(keyCode, event); //不再往下执行。
+        }
+
+        map.setMapStatus(update);
+        return super.onKeyDown(keyCode, event);
+    }
 
     //监听百度Key配置是否正确
     private void registerSDKCheckReceiver() {
